e844e110fe033a0d53db0c9eebda902ac6c3a07a
Ahmad Deel <39188688+AhmadDeel@users.noreply.github.com>
2019-12-05 00:17:05 +0330

    Update C# challenge 4 - smallest word in the given string.cs

--- C# challenge 4 - smallest word in the given string.cs
+++ C# challenge 4 - smallest word in the given string.cs
@@ -3,6 +3,8 @@ public class Kata
 {
   public static int FindShort(string s)
   {
-    return s.Split(new char[] { ' ' }).OrderBy(c => c.Length).ToArray()[0].Length;
+    return s.Split(' ').OrderBy(c => c.Length).ToArray()[0].Length;
+    //return s.Split(' ').Min(s1 => s1.Length );
+    //return s.Split(' ').OrderBy(i => i.Length).Select(i=>i.Length).First();
   }
 }
